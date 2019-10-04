ac0527cd6e154fd578276c54e6afddbf5a2b5342
Bernhard Stöcker <bernhard.stoecker@aktivweb.de>
2019-10-04 08:49:14 +0200

    init CppStringFormatTag.java

---
+++ src/model/CppStringFormatTag.java
@@ -0,0 +1,29 @@
+package model;
+
+import config.TemplateConfig;
+import io.parser.HtmlParser;
+
+public class CppStringFormatTag extends HtmlTag {
+	public static final String TAG_NAME = "string" ;
+	
+	public CppStringFormatTag() {
+		super(TAG_NAME);
+		setNs(HtmlParser.CPP_NS);
+	}
+	
+	@Override
+	public void toCpp(StringBuilder out, StringBuilder directTextOutputBuffer, TemplateConfig cfg) {
+		/*CppOutput.clearDirectTextOutputBuffer(out, directTextOutputBuffer, cfg);
+		
+		if(childNodes.size() == 1) {
+			if(childNodes.get(0) instanceof TextNode) {
+				TextNode t = (TextNode) childNodes.get(0);
+				CodeUtil.writeLine(out, "QStringLiteral" + CodeUtil.quote(CodeUtil.parentheses(t.getText()))+".args"
+			}
+		}
+		
+		CodeUtil.writeLine(out, "FastCgiOutput::write"+CodeUtil.parentheses("translations->"+ getAttrByName("key").getStringValue()+"().toHtmlEscaped(), out")+";");
+		cfg.setIncludeTranslations(true);*/
+	}
+
+}
